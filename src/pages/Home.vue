d0149ba9bee3f3bcdaaade45b6d99c199f73d2a6
Benjamin Hoffmann <44234556+bhffmnn@users.noreply.github.com>
2021-04-30 09:46:43 +0200

    add how-to

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,48 +1,67 @@
 <template>
   <div class="flexbox-columns-spaced">
-    <search-bar @searchResults="getResults"/>
-    <result-bar :results="result"/>
+    <search-bar @searchResults="getResults" />
+    <result-bar :results="result" />
     <div>
-      <kanji-display-box v-if="kanjiObject.literal" :kanji="kanjiObject"/>
+      <kanji-display-box v-if="kanjiObject.literal" :kanji="kanjiObject" />
+      <div v-else>
+        <h4>Two methods to search:</h4>
+        <ol class="flexbox-columns-spaced">
+          <li>
+            By text: Type a single kanji, an on-reading in katakana, a
+            kun-reading in hiragana or an English meaning into the search bar
+            and hit 'Search'. If your search term yields multiple results you
+            can choose the desired character from the box below the search bar.
+          </li>
+          <li>
+            By components: Click on "Parts" to open the component search box.
+            Choose the components the kanji you're search for is composed of,
+            choose the desired character from the results that are displayed
+            above and hit 'Search'.
+          </li>
+        </ol>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import SearchBar from '@/components/SearchBar' 
-import ResultBar from '@/components/ResultBar'
-import KanjiDisplayBox from '@/components/KanjiDisplayBox'
+import SearchBar from "@/components/SearchBar";
+import ResultBar from "@/components/ResultBar";
+import KanjiDisplayBox from "@/components/KanjiDisplayBox";
 
 export default {
   props: {
     kanji: {
       required: false,
-      type: String
-    }
+      type: String,
+    },
   },
   data() {
     return {
       result: [],
       kanjiObject: {},
-    }
+    };
   },
   components: {
     SearchBar,
     ResultBar,
-    KanjiDisplayBox
+    KanjiDisplayBox,
   },
   methods: {
     getResults(searchResults) {
       this.result = searchResults;
       if (this.result.length > 0) {
-        this.$router.push({ name: 'Home', params: { kanji: this.result[0] } })
-        this.$router.go(1)
+        this.$router.push({ name: "Home", params: { kanji: this.result[0] } });
+        this.$router.go(1);
       }
     },
     async getKanjiObject() {
-      const response = await fetch('http://localhost:5000/api/kanji/' + this.kanji);
+      const response = await fetch(
+        "http://localhost:5000/api/kanji/" + this.kanji
+      );
       this.kanjiObject = await response.json();
-    }
+    },
   },
   created() {
     if (this.kanji) {
@@ -52,10 +71,9 @@ export default {
   watch: {
     kanji: function() {
       this.getKanjiObject();
-    }     
-  }
-}
+    },
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
