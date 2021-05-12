8f0cafbec90a06a41dc204e28b565a149cd0e33d
Benjamin Hoffmann <44234556+bhffmnn@users.noreply.github.com>
2021-05-12 15:33:02 +0200

    fix flexbox

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flexbox f-columns" id="main-box">
+  <div class="flexbox f-columns flex-grow" id="main-box">
     <the-header/>
     <router-view/>
     <div class="footer">
@@ -22,18 +22,14 @@ export default {
 <style>
 #app {
   display: flex;
-  flex-direction: column;
+  justify-content: center;
   height: 100%;
 }
 #main-box {
   max-width: 1000px;
-  margin-top :800;
-  margin: auto;
+  margin-top : 800;
   padding-left: 10px;
   padding-right: 10px;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
 }
 * {
   font-family : 'ヒラギノ角ゴ ProN', 'Hiragino Kaku Gothic ProN', '游ゴシック', '游ゴシック体', YuGothic, 'Yu Gothic', 'メイリオ', Meiryo, 'ＭＳ ゴシック', 'MS Gothic', HiraKakuProN-W3, 'TakaoExゴシック', TakaoExGothic, 'MotoyaLCedar', 'Droid Sans Japanese', sans-serif;
